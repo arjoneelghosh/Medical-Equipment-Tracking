ffe19db3553437507a2982a35c52b904e5f3b787
Arjoneel Ghosh <arjoneelghosh03@gmail.com>
2025-06-10 22:37:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/catalog" element={<Catalog />} />
+            
             <Route path="/product/:id" element={<ProductDetail />} />
+            
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
